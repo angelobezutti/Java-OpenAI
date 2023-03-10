d8082dbf18329da4cff7244557f2bfad7f4b3c0f
Angelo Antonio Bezutti <angelo.bezutti@sysmap.com.br>
2023-03-10 10:46:02 -0300

    Init

---
+++ src/main/java/com/angelobezutti/javaopenai/JavaOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.javaopenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelobezutti/javaopenai/JavaOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.javaopenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
